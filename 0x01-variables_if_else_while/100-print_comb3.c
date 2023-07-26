884625271b869e13e7ffe338810c3dd7c2417b6d
omarsalaharafa <xxbakarxxsalah2001@gmail.com>
2023-07-26 08:25:58 -0700

    done

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -15,18 +15,20 @@ while (digit1 <= 9)
 {
 digit2 = 0;
 while (digit2 <= 9)
-
-if (digit1 != digit2 & digit1 < digit2)
+{
+if (digit1 != digit2 && digit1 < digit2)
 {
 putchar (digit1 + 48);
 putchar (digit2 + 48);
 if (digit1 + digit2 != 17)
 putchar (',');
 putchar (' ');
-
 }
+
 digit2++;
+}
 digit1++;
+}
 
 putchar('\n');
 
