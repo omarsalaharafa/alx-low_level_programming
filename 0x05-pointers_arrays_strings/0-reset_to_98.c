a5a637dafc4f80feb380fd785576801040fcb310
omarsalaharafa <xxbakarxxsalah2001@gmail.com>
2023-07-28 16:14:07 +0300

    Merge remote 'main' branch and resolve conflicts

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * rest_to_98 - a function that takes a pointer to in as parameter
+ * and updates the value it points to 98
+ *
+ * @n: input
+ *
+ * Return: n
+*/
+
+void rest_to_98(int *n)
+{
+		*n = 98;
+}
